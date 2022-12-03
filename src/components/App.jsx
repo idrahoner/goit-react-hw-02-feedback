28282e984c6795428393e389c8c8446d2969de52
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2022-12-03 19:59:49 +0200

    Working version of Feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import Feedback from 'components/Feedback';
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Feedback />
     </div>
   );
 };
